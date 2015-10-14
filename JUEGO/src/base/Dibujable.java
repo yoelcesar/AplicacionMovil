12b691c4d57f364ad47b62b3fe49a815541d9d25
yoelcesar <yoelruiztaipe@gmail.com>
2015-10-14 07:48:29 -0500

    segundo comit

---
+++ JUEGO/src/base/Dibujable.java
@@ -0,0 +1,10 @@
+package base;
+
+import java.awt.Graphics;
+
+//esta interfaz debe ser implementada por todos los bjetos graficos del juego
+public interface Dibujable {
+
+	public void dibuja(Graphics g);
+	
+}
